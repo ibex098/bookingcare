73948870432b43ab1e4f964846aa6316979012b1
kuli098 <nhan120298@gmail.com>
2022-07-14 21:07:53 +0700

    first cm

---
+++ src/controllers/homeController.js
@@ -0,0 +1,17 @@
+import { json } from 'body-parser';
+import db from '../models/index';
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    return res.render('homepage.ejs', {
+      data: JSON.stringify(data),
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  getHomePage: getHomePage,
+};
